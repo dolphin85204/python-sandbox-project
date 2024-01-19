d900c54f03290ea04d7bbfab356f1acc1c896879
William Terry <dolphin85204@me.com>
2024-01-18 21:03:36 -0700

    Added trivial initial files

---
+++ main.py
@@ -0,0 +1,12 @@
+from welcome_class import Welcome
+
+def main():
+    
+    greet = Welcome()
+    greet.getName()
+    greet.greet()
+    
+if __name__ == '__main__':
+    main()    
+  
+
---
+++ welcome_class.py
@@ -0,0 +1,19 @@
+
+
+class Welcome:
+    
+    def __init__(self, *args, **kwargs):
+        
+        # this really isn't doing anything right now.
+        self.name = "";
+        
+    def getName(self):
+        
+        print("\n")
+        self.name = str(input('\tWhat is your name? '))
+        print("\n")
+       
+    def greet(self):
+        
+        print("\tHello {}, let's get started.".format(self.name))    
+        print("\n")
